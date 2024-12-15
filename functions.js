97350a64732b8e3042175818a0f633a09dda2d9e
yaguchiharuto <haruto.yaguchi.7c@stu.hosei.ac.jp>
2024-12-16 00:03:25 +0900

    add

---
+++ functions.js
@@ -0,0 +1,31 @@
+gsap.registerPlugin(scrollTrigger);
+
+const imageContainer =document.querySelector('.image-container');
+const layers =document.querySelectorAll('.image > *');
+
+gsap.set(layers,{opacity:0});
+gsap.set('#layer01',{opacity:1});
+
+scrollTrigger.create({
+    trigger:imageContainer,
+    start:'top top',
+    end:'+=2000',
+    pin:true,
+    scrub:true,
+   // markers:true,
+   onUpdate:(self) => {
+    if(self.progress<0.25){
+        gsap.to('#layer01',{opacity:1});
+        gsap.to('#layer02,#layer03, #layer04',{opacity:0});
+    } else if(self.progress >= 0.25 && self.progress <0.5){
+        gsap.to('#layer01, #layer02',{opacity:1});
+        gsap.to('#layer03,#layer04',{opacity:0});
+    } else if (self.progress >= 0.5 && self.progress < 0.75){
+        gsap.to('#layer01,#layer02,#layer03',{opacity:1});
+        gsap.to('#layer04',{opacity:0});
+    } else {
+        gsap.to('#layer01,#layer02,#layer03,#layer04',{opacity:1});
+
+    }
+   },
+});
